cb3697f310a8bb38df7ff2dbc4e9772dc29a510c
Ignis-g <105871593+Ignis-g@users.noreply.github.com>
2023-10-18 11:21:44 +0200

    Update journey_map.py (Refactoring)

--- journey_map.py
+++ journey_map.py
@@ -3,117 +3,137 @@ from PIL import Image, ImageTk, ImageDraw
 from ReadWriteMemory import ReadWriteMemory
 from struct import unpack
 
-def load_image(level_id):
-    global image, img_label, window_size
-    image = Image.open(f"level_0{level_id}.webp")
-    image_resized = ImageTk.PhotoImage(image.resize((window_size[0], window_size[1]), Image.LANCZOS))
-    img_label.config(image=image_resized)
-    img_label.image = image_resized
+# Constants
+WAYFARER_COLOR = 'yellow'
+NICK_COLOR = 'red'
 
-def load_dot_coordinates():
-    global wayfarer_pointers, nick_pointers
-    
-    wayfarer_position = (unpack('<f', process.read(wayfarer_pointers[0]).to_bytes(4, 'little'))[0], 
-                    unpack('<f', process.read(wayfarer_pointers[1]).to_bytes(4, 'little'))[0])
-    nick_position = (unpack('<f', process.read(nick_pointers[0]).to_bytes(4, 'little'))[0], 
-                    unpack('<f', process.read(nick_pointers[1]).to_bytes(4, 'little'))[0])
+LEVEL_ADDS = {
+    0: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
+    1: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
+    2: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
+    3: {'z': 520, 'x': 1785, 'x_step': 3.0, 'z_step': 2.985},
+    4: {'z': 255, 'x': 1845, 'x_step': 3.21, 'z_step': 3.2},
+    5: {'z': 405, 'x': 1850, 'x_step': 3.2, 'z_step': 3.2},
+    6: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
+    7: {'z': 780, 'x': 1660, 'x_step': 2.45, 'z_step': 2.48},
+}
 
-    return [wayfarer_position, nick_position]
-    
-
-def draw_dots():
-    global image, img_label, window_size, current_level_id
-    
-    if image:
-        image_draw = image.copy()
-        draw = ImageDraw.Draw(image_draw)
-        coordinates = load_dot_coordinates()
-
-        pen_size = 8
+class JourneyApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Journey")
+        self.root.geometry(f"{self.root.winfo_screenwidth()}x{self.root.winfo_screenheight()}")
+        self.root.state('zoomed')
         
-        level_adds = {
-            0: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
-            1: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
-            2: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
-            3: {'z': 520, 'x': 1785, 'x_step': 3.0, 'z_step': 2.985},
-            4: {'z': 255, 'x': 1845, 'x_step': 3.21, 'z_step': 3.2},
-            5: {'z': 405, 'x': 1850, 'x_step': 3.2, 'z_step': 3.2},
-            6: {'z': 410, 'x': 1840, 'x_step': 3.2, 'z_step': 3.2},
-            7: {'z': 780, 'x': 1660, 'x_step': 2.45, 'z_step': 2.48},
-        }
-
+        self.window_size = (self.root.winfo_screenwidth(), self.root.winfo_screenheight())
         
-        z_add = level_adds[current_level_id]['z']
-        x_add = level_adds[current_level_id]['x']
+        self.process = None
+        self.image = None
+        self.current_level_id = -1
 
-        z_step = level_adds[current_level_id]['z_step']
-        x_step = level_adds[current_level_id]['x_step']
+        self.create_gui()
+        self.connect_to_process()
+        self.update()
 
-        wayfarer_color = 'yellow'
-        wayfarer_x, wayfarer_z = coordinates[0]
+    def create_gui(self):
+        empty_image = tk.PhotoImage(width=1, height=1)
+        self.img_label = tk.Label(self.root, image=empty_image)
+        self.img_label.pack(fill=tk.BOTH, expand=True)
+        self.root.bind("<Configure>", self.resize_canvas)
 
-        nick_color = 'red'
-        nick_x, nick_z = coordinates[1]
+    def connect_to_process(self):
+        try:
+            self.rwm = ReadWriteMemory()
+            self.process = self.rwm.get_process_by_name('Journey.exe')
+            self.process.open()
+            self.base_address = self.process.get_base_address()
+            self.level_id_pointer = self.process.get_pointer(self.base_address + 0x3CFCA80, offsets=[0x70, 0x28, 0xD0, 0x100, 0x30, 0x368, 0x30])
+        except Exception as e:
+            print(f"Error connecting to process: {e}")
+            self.root.destroy()
 
-        draw.ellipse((z_add + wayfarer_z * z_step - pen_size, x_add - wayfarer_x * x_step - pen_size, z_add + wayfarer_z * z_step + pen_size, x_add - wayfarer_x * x_step + pen_size), fill=wayfarer_color)
-        draw.ellipse((z_add + nick_z * z_step - pen_size, x_add - nick_x * x_step - pen_size, z_add + nick_z * z_step + pen_size, x_add - nick_x * x_step + pen_size), fill=nick_color)
+    def load_image(self, level_id):
+        try:
+            image = Image.open(f"level_0{level_id}.webp")
+            image_resized = ImageTk.PhotoImage(image.resize((self.window_size[0], self.window_size[1]), Image.LANCZOS))
+            self.img_label.config(image=image_resized)
+            self.img_label.image = image_resized
+            self.image = image
+        except Exception as e:
+            print(f"Error loading image: {e}")
 
-        image_resized = ImageTk.PhotoImage(image_draw.resize((window_size[0], window_size[1]), Image.LANCZOS))
-        img_label.config(image=image_resized)
-        img_label.image = image_resized
+    def load_dot_coordinates(self):
+        wayfarer_pointer_offsets = ([0x60, 0x28, 0xD0, 0x100, 0x30, 0x370, 0xC0], [0x70, 0x28, 0xD0, 0x108, 0x30, 0x370, 0xC8])
+        nick_pointer_offsets = ([0x60, 0x28, 0xD0, 0x108, 0x4A0, 0x10, 0x948], [0x60, 0x28, 0xD0, 0x108, 0x4A0, 0x10, 0x950])
 
-def resize_canvas(event):
-    global window_size
+        def read_coordinates(pointer, offsets):
+            position = []
+            address = self.base_address + pointer
 
-    width = event.width
-    height = event.height
+            for offset in offsets:
+                current_address = self.process.get_pointer(address, offsets=offset)
+                position.append(unpack('<f', self.process.read(current_address).to_bytes(4, 'little'))[0])
+        
+            return tuple(position)
 
-    if (width, height) != window_size:
-        window_size = (width, height)
+        wayfarer_position = read_coordinates(0x3C47B18, wayfarer_pointer_offsets)
+        nick_position = read_coordinates(0x3C47B18, nick_pointer_offsets)
 
+        return [wayfarer_position, nick_position]
 
-def update():
-    global current_level_id, level_id_pointer
+    def draw_dots(self):
+        if self.image is not None:
+            image_draw = self.image.copy()
+            draw = ImageDraw.Draw(image_draw)
+            coordinates = self.load_dot_coordinates()
 
-    new_level_id = process.read(level_id_pointer)
-    if new_level_id != current_level_id:
-        current_level_id = new_level_id
-        load_image(current_level_id)
+            pen_size = 8
+            
+            z_add = LEVEL_ADDS.get(self.current_level_id, {}).get('z', 0)
+            x_add = LEVEL_ADDS.get(self.current_level_id, {}).get('x', 0)
+            z_step = LEVEL_ADDS.get(self.current_level_id, {}).get('z_step', 1.0)
+            x_step = LEVEL_ADDS.get(self.current_level_id, {}).get('x_step', 1.0)
 
-    draw_dots()
-    root.after(1000, update)
+            wayfarer_color = WAYFARER_COLOR
+            wayfarer_x, wayfarer_z = coordinates[0]
 
+            nick_color = NICK_COLOR
+            nick_x, nick_z = coordinates[1]
 
+            draw.ellipse((z_add + wayfarer_z * z_step - pen_size, x_add - wayfarer_x * x_step - pen_size,
+                          z_add + wayfarer_z * z_step + pen_size, x_add - wayfarer_x * x_step + pen_size), fill=WAYFARER_COLOR)
 
-rwm = ReadWriteMemory()
-process = rwm.get_process_by_name('Journey.exe')
-process.open()
-base_address = process.get_base_address()
-level_id_pointer = process.get_pointer(base_address + 0x3CFCA80, offsets=[0x70, 0x28, 0xD0, 0x100, 0x30, 0x368, 0x30])
-wayfarer_pointers = (process.get_pointer(base_address + 0x3C47B18, offsets=[0x60, 0x28, 0xD0, 0x100, 0x30, 0x370, 0xC0]),
-                    process.get_pointer(base_address + 0x3C47B18, offsets=[0x70, 0x28, 0xD0, 0x108, 0x30, 0x370, 0xC8]))
+            draw.ellipse((z_add + nick_z * z_step - pen_size, x_add - nick_x * x_step - pen_size,
+                          z_add + nick_z * z_step + pen_size, x_add - nick_x * x_step + pen_size), fill=NICK_COLOR)
 
-nick_pointers = (process.get_pointer(base_address + 0x3C47B18, offsets=[0x60, 0x28, 0xD0, 0x108, 0x4A0, 0x10, 0x948]),
-                process.get_pointer(base_address + 0x3C47B18, offsets=[0x60, 0x28, 0xD0, 0x108, 0x4A0, 0x10, 0x950]))
+            image_resized = ImageTk.PhotoImage(image_draw.resize((self.window_size[0], self.window_size[1]), Image.LANCZOS))
+            self.img_label.config(image=image_resized)
+            self.img_label.image = image_resized
 
-root = tk.Tk()
-root.title("Journey")
+    def resize_canvas(self, event):
+        width = event.width
+        height = event.height
 
-window_size = (root.winfo_screenwidth(), root.winfo_screenheight())
-root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
-root.state('zoomed')
+        if (width, height) != self.window_size:
+            self.window_size = (width, height)
 
-empty_image = tk.PhotoImage(width=1, height=1)
+    def update(self):
+        new_level_id = self.process.read(self.level_id_pointer)
+        if new_level_id != self.current_level_id:
+            self.current_level_id = new_level_id
+            self.load_image(self.current_level_id)
 
-img_label = tk.Label(root, image=empty_image)
-img_label.pack(fill=tk.BOTH, expand=True)
+        self.draw_dots()
+        self.root.after(1000, self.update)
 
-current_level_id = -1
-image = None
+    def run(self):
+        self.root.mainloop()
 
-root.bind("<Configure>", resize_canvas)
+    def close(self):
+        if self.process:
+            self.process.close()
 
-update()
-
-root.mainloop()
-process.close()
+if __name__ == "__main__":
+    app = JourneyApp()
+    app.run()
+    app.close()
